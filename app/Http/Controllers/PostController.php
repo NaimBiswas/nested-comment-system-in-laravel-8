386047a383b66dc1d56f9511fe67ed66567411ff
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-05 16:00:33 +0600

    Create a store function and valited the post title, and create post with slug

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,5 +12,20 @@ class PostController extends Controller
     {
         $post = Post::take(5)->get();
         return view('post.index', compact('post'));
+    }
+    public function create()
+    {
+        return view('post.create');
+    }
+    public function store(Request $request)
+    {
+        $validator = $request->validate([
+            'title' => 'required|max:100|min:10',
+        ]);
+        Post::create([
+            'title' => $request->title,
+            'slug' => Str::slug($request->title),
+        ]);
+        return redirect()->back();
     }
 }
