efce4a5d600cad4ba110502aa5795c28fe0d9e3a
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-05 15:41:17 +0600

    Add Relation Comment to user and Comment to replies that is a many to many relationship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,4 +9,12 @@ class Comment extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
 }
