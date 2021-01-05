e67d6001c509a9fb70f08631d053faa6bdc0d44d
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-05 15:38:22 +0600

    Add Relation between Post to user and Post to comments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,12 @@ class Post extends Model
 {
     use HasFactory;
     protected $guarded = ['title', 'slug'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable')->whereNull('parent_id');
+    }
 }
