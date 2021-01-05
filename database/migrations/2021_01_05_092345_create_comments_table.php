8bbe50ee7e488f611fce615d123d5cfbf33d5223
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-05 15:27:47 +0600

    Comment table created

--- database/migrations/2021_01_05_092345_create_comments_table.php
+++ database/migrations/2021_01_05_092345_create_comments_table.php
@@ -15,6 +15,11 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->integer('parent_id')->unsigned();
+            $table->text('comment');
+            $table->integer('commentable_id')->unsigned();
+            $table->string('commentable_type');
             $table->timestamps();
         });
     }
