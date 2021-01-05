5069234af5b4df85c718a1251b22f81717ec1eec
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-05 16:11:45 +0600

    Create Comment Store function and store the comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -15,6 +17,11 @@ class CommentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $comment = new Comment();
+        $comment->comment = $request->comment;
+        $comment->user()->associate($request->user());
+        $post = Post::find($request->post_id);
+        $post->comments()->save($comment);
+        return back();
     }
 }
